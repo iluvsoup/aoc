819c53082da28de367f6a0560d87969ba774fe44
iluvsoup <94307595+iluvsoup@users.noreply.github.com>
2023-12-09 23:19:43 +0100

    Add day 9

---
+++ 2023/go/09/b.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	bytes, err := os.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	lines := strings.Split(string(bytes), "\n")
+
+	var sum int
+
+	for _, line := range lines {
+		if line != "" {
+			numberStrings := strings.Split(line, " ")
+			var numbers []int
+			for _, numberString := range numberStrings {
+				number, err := strconv.Atoi(numberString)
+				if err != nil {
+					panic(err)
+				}
+				numbers = append(numbers, number)
+			}
+
+			d := numbers
+			var differenceSequences [][]int
+			for {
+				allZeroes := true
+				for _, v := range d {
+					if v != 0 {
+						allZeroes = false
+						break
+					}
+				}
+				if allZeroes {
+					break
+				} else {
+					differenceSequences = append(differenceSequences, d)
+				}
+
+				d = getDifferences(d)
+			}
+
+			previousValue := 0
+			for i := len(differenceSequences) - 1; i >= 0; i-- {
+				differences := differenceSequences[i]
+				newValue := differences[0] - previousValue
+				previousValue = newValue
+			}
+			sum += previousValue
+		}
+	}
+
+	fmt.Println(sum)
+}
+
+func getDifferences(numbers []int) []int {
+	var differences []int
+	for i := 0; i < len(numbers)-1; i++ {
+		number := numbers[i]
+		nextNumber := numbers[i+1]
+
+		diff := nextNumber - number
+		differences = append(differences, diff)
+	}
+	return differences
+}
